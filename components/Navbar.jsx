dc1c749aea593711bd1401a745fbdd5410fcb98c
wassim <wassimatmo@gmail.com>
2025-05-11 21:37:07 +0200

    navbar to main route

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,9 @@ export default function Navbar() {
     return (
       <nav className="w-full border-b p-4">
         <div className="max-w-6xl mx-auto flex justify-between items-center">
-          <span className="text-xl font-bold">exAImen</span>
+        <Link href="/" className="text-xl font-bold hover:opacity-80 transition">
+          exAImen
+        </Link>
           <div className="space-x-4 text-sm">
             {/*<a href="/auth/login" className="hover:underline">Login</a>
             <a href="/dashboard" className="hover:underline">Dashboard</a>*/}
